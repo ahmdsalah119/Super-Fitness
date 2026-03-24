58cf7f759766736f4c9eb8fb4be948d0d339f631
M-hossameldine <m.hossameldine@gmail.com>
2026-03-24 06:06:50 +0200

    feat(login): add AppInput and ErrorBox shared components

---
+++ src/components/shared/app-input.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useIntl } from "react-intl";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Button } from "@/components/ui/button";
+
+import { Mail, Lock, Eye, EyeOff, type LucideIcon } from "lucide-react";
+
+const inputConfigs = {
+  email: {
+    icon: Mail,
+    placeholder: "input.email",
+  },
+  password: {
+    icon: Lock,
+    placeholder: "input.password",
+  },
+};
+
+type AppInputProps = React.ComponentProps<"input"> & {
+  icon?: LucideIcon;
+};
+
+export function AppInput(props: AppInputProps) {
+  // Translations
+  const { formatMessage } = useIntl();
+
+  // States
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  // Variables
+  const inputConfig = inputConfigs[props.type as keyof typeof inputConfigs];
+  const Icon = props.icon || inputConfig?.icon;
+  const isPassword = props.type === "password";
+
+  return (
+    <InputGroup>
+      {/* Addon Icon */}
+      <InputGroupAddon>{Icon && <Icon />}</InputGroupAddon>
+
+      {/* Input */}
+      <InputGroupInput
+        {...props}
+        type={isPassword && isPasswordVisible ? "text" : props.type}
+        placeholder={formatMessage({ id: inputConfig?.placeholder })}
+      />
+
+      {/* Password Visibility Toggle */}
+      {isPassword && (
+        <InputGroupAddon align="inline-end">
+          <Button
+            variant="ghost"
+            size="icon"
+            type="button"
+            onClick={() => setIsPasswordVisible((prev) => !prev)}
+          >
+            {isPasswordVisible ? <EyeOff /> : <Eye />}
+          </Button>
+        </InputGroupAddon>
+      )}
+    </InputGroup>
+  );
+}
