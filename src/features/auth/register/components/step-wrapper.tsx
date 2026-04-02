fba7b7f16594b52d618f8291037cf508844ea9cd
Gamal Saed <gamalsaed557@gmail.com>
2026-04-02 19:32:04 +0200

    fixed folder structure

--- src/features/auth/register/components/step-wrapper.tsx
+++ src/features/auth/register/components/step-wrapper.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowBigLeft } from "lucide-react";
-import { Link } from "react-router-dom";
 
 type StepWrapperProps = {
   title?: string;
