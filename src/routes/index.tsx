ed179d4dd0da37ad87e1f3e43ad72e85eae76190
Gamal Saed <gamalsaed557@gmail.com>
2026-04-07 20:17:32 +0200

    added hero section, fixed shadcn batton component and fixed the font in tailwind.config.js

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "@/features/auth/login";
-
+import HeroSection from "@/features/hero-section";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <div className="p-10">
-        <h1 className="text-2xl  underline">Tailwind is working</h1>
+        <h1 className="text-2xl  underline">Tailwind is Working</h1>
       </div>
     ),
   },
