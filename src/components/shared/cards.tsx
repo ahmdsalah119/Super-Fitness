0b7346ba0829ae1dbb638e7f6cbb510ac49c1128
Salma Alzahdy <salmaalzahdy@gmail.com>
2026-04-18 02:34:36 +0200

    fix(ui): fixed up the card component

--- src/components/shared/cards.tsx
+++ src/components/shared/cards.tsx
@@ -6,23 +6,33 @@ interface CardProps {
   title: string;
   img: string;
 }
-
 export default function Cards({ title, img }: CardProps) {
   return (
     <div className="flex items-center justify-center">
-      <Card className="relative w-64 h-64">
-        {/* image part */}
-        <div className=" h-64 w-64 rounded-lg overflow-hidden">
+      <Card className="relative w-64 h-64 border-none shadow-lg">
+        {/* image */}
+        <div className="h-64 w-64 rounded-lg overflow-hidden">
           <img src={img} alt={title} className="w-full h-full object-cover" />
         </div>
-        {/* bottom part */}
-        <div className="absolute bottom-0 w-full bg-card/80 backdrop-blur-md p-4 ">
-          <h3 className="text-left">{title}</h3>
-          {/* explore button */}
-          <div className=" flex items-center justify-between">
-            <p className="text-primary">Explore </p>
-            <Button size="icon" className="bg-primary rounded-full h-8 w-8">
-              <ArrowRight className="h-4 w-4 text-white" />
+
+        {/* overlay */}
+        <div className="absolute h-16 bottom-0 w-full bg-card/80 backdrop-blur-md px-4 py-2 rounded-b-lg flex flex-col justify-between">
+          {/* title */}
+          <h3 className="text-left text-secondary text-sm font-medium truncate">
+            {title}
+          </h3>
+
+          {/* explore */}
+          <div className="flex items-center">
+            <p className="text-primary text-xs font-semibold">Explore</p>
+            <Button
+              size="icon"
+              className="bg-primary rounded-full h-7 w-7 ml-2 hover:bg-primary/90"
+            >
+              <ArrowRight
+                className="h-2 w-2 text-black dark:text-white -rotate-45"
+                strokeWidth={1}
+              />
             </Button>
           </div>
         </div>
